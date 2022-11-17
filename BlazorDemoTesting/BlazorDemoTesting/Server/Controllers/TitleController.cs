825332db30e3d1bfb6f01d313e734ef3b03300b7
Antonio Venditti <antonio.venditti@blexin.com>
2022-11-17 09:16:31 +0100

    blazor components

---
+++ BlazorDemoTesting/BlazorDemoTesting/Server/Controllers/TitleController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDemoTesting.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TitleController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public TitleController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var title = configuration.GetValue<string>("title");
+
+            return Ok(title);
+        }
+    }
+}
